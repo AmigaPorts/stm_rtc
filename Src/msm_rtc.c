7935f66e628356e08a81f36e31c2486e9492b3db
KaiN <kain@piwnica.ws>
2020-09-03 14:23:34 +0200

    tried to write asm by hand

--- Src/msm_rtc.c
+++ Src/msm_rtc.c
@@ -62,48 +62,241 @@ void msmRtcInit(void) {
 	);
 
 	// Print hello message
-	printf(
-		"RTC_INIT: %lu%lu.%lu%lu.%lu%lu %lu%lu:%lu%lu:%lu%lu\r\n",
-		// Date
-		((RTC->DR & RTC_DR_DT_Msk) >> RTC_DR_DT_Pos),
-		((RTC->DR & RTC_DR_DU_Msk) >> RTC_DR_DU_Pos),
-		((RTC->DR & RTC_DR_MT_Msk) >> RTC_DR_MT_Pos),
-		((RTC->DR & RTC_DR_MU_Msk) >> RTC_DR_MU_Pos),
-		((RTC->DR & RTC_DR_YT_Msk) >> RTC_DR_YT_Pos),
-		((RTC->DR & RTC_DR_YU_Msk) >> RTC_DR_YU_Pos),
-		// Time
-		((RTC->TR & RTC_TR_HT_Msk) >> RTC_TR_HT_Pos),
-		((RTC->TR & RTC_TR_HU_Msk) >> RTC_TR_HU_Pos),
-		((RTC->TR & RTC_TR_MNT_Msk) >> RTC_TR_MNT_Pos),
-		((RTC->TR & RTC_TR_MNU_Msk) >> RTC_TR_MNU_Pos),
-		((RTC->TR & RTC_TR_ST_Msk) >> RTC_TR_ST_Pos),
-		((RTC->TR & RTC_TR_SU_Msk) >> RTC_TR_SU_Pos)
-	);
+	// printf(
+	// 	"RTC_INIT: %lu%lu.%lu%lu.%lu%lu %lu%lu:%lu%lu:%lu%lu\r\n",
+	// 	// Date
+	// 	((RTC->DR & RTC_DR_DT_Msk) >> RTC_DR_DT_Pos),
+	// 	((RTC->DR & RTC_DR_DU_Msk) >> RTC_DR_DU_Pos),
+	// 	((RTC->DR & RTC_DR_MT_Msk) >> RTC_DR_MT_Pos),
+	// 	((RTC->DR & RTC_DR_MU_Msk) >> RTC_DR_MU_Pos),
+	// 	((RTC->DR & RTC_DR_YT_Msk) >> RTC_DR_YT_Pos),
+	// 	((RTC->DR & RTC_DR_YU_Msk) >> RTC_DR_YU_Pos),
+	// 	// Time
+	// 	((RTC->TR & RTC_TR_HT_Msk) >> RTC_TR_HT_Pos),
+	// 	((RTC->TR & RTC_TR_HU_Msk) >> RTC_TR_HU_Pos),
+	// 	((RTC->TR & RTC_TR_MNT_Msk) >> RTC_TR_MNT_Pos),
+	// 	((RTC->TR & RTC_TR_MNU_Msk) >> RTC_TR_MNU_Pos),
+	// 	((RTC->TR & RTC_TR_ST_Msk) >> RTC_TR_ST_Pos),
+	// 	((RTC->TR & RTC_TR_SU_Msk) >> RTC_TR_SU_Pos)
+	// );
 	D0_GPIO_Port->MODER = 0;
 }
 
-static uint32_t s_pMsmFields[MSM_ADDR_COUNT] = {
+static volatile uint32_t s_pMsmFields[MSM_ADDR_COUNT] = {
 	 0 << D0_Pos,  1 << D0_Pos,  2 << D0_Pos,  3 << D0_Pos,
 	 4 << D0_Pos,  5 << D0_Pos,  6 << D0_Pos,  7 << D0_Pos,
 	 8 << D0_Pos,  9 << D0_Pos, 10 << D0_Pos, 11 << D0_Pos,
 	12 << D0_Pos, 13 << D0_Pos, 14 << D0_Pos, 15 << D0_Pos
 };
 
-static inline __attribute__((always_inline)) void rtcReadWriteProcess(void) {
-	uint32_t ulStatus = NRD_GPIO_Port->IDR;
-	// NWR/NRD may not occur right on CS edge, thus check for both pins at once
-	if((ulStatus & WRITE_MASK) == 0) {
-		// Write - TIME CRITICAL!
-		uint8_t ubAddr = A0_GPIO_Port->IDR >> A0_Pos; // read addr asap
-		uint8_t ubData = D0_GPIO_Port->IDR >> D0_Pos; // read data asap
-		// printf("WRITE: %x %x\r\n", ubAddr, ubData);
+// #define USE_ASM
+
+#if defined(USE_ASM)
+
+const uint16_t uwModerDataOut = (
+	MODE_OUT(D0_Pos + 0) | MODE_OUT(D0_Pos + 1) |
+	MODE_OUT(D0_Pos + 2) | MODE_OUT(D0_Pos + 3)
+);
+volatile const uint16_t uwWriteMask = WRITE_MASK;
+volatile const uint16_t uwReadMask = READ_MASK;
+volatile GPIO_TypeDef *pPortD0 = D0_GPIO_Port;
+volatile GPIO_TypeDef *pPortA0 = A0_GPIO_Port;
+
+static inline __attribute__((always_inline)) void rtcInitAsm(void) {
+	// Done once, r9-r3 are permanently used up
+	__asm__ __volatile__(
+		".thumb \n"
+		"ldr r0, =uwModerDataOut \n"
+		"mov r9, r0 \n"
+		"ldr r0, =s_pMsmFields \n"
+		"mov r8, r0 \n"
+		"ldr r7, =pPortD0 \n"
+		"ldr r6, =pPortA0 \n"
+		"ldr r5, =uwWriteMask \n"
+		"ldr r4, =uwReadMask \n"
+		"movs r3, %0\n"
+		::
+			"i"(0xF << 2)
+	);
+}
+
+static inline __attribute__((always_inline)) void rtcReadWriteProcessAsm(void) {
+	const uint8_t ubOffsA0 = A0_Pos - 2;
+	const uint16_t uwOffsModer = offsetof(GPIO_TypeDef, MODER);
+	const uint16_t uwOffsOdr = offsetof(GPIO_TypeDef, ODR);
+	const uint16_t uwOffsIdr = offsetof(GPIO_TypeDef, IDR);
+	__asm__ __volatile__ (
+		".thumb \n"
+		".syntax unified \n"
+		".2: \n"
+		"ldr r0, [r6, %3] \n"
+		"tst r0, r4 \n"
+		"beq .1 \n"
+
+		"tst r0, r5 \n"
+		"bne .3 \n"
+
+		// WRITE stuff
+		"b .3 \n"
+
+		// READ stuff
+		".1: \n"
+		// "ldr r0, [r6] \n" // not needed since read with status bits
+		"lsrs r0, %0 \n"
+		"ands r0, r3 \n"
+		"add r0, r8 \n"
+		"ldr r0, [r0] \n"
+		"str r0, [r7, %2] \n" // output value
+		"mov r0, r9 \n"
+		"str r0, [r7, %1] \n" // set to "out"
+		"movs r0, #0 \n"
+		"str r0, [r7, %1] \n" // set to "in" once again
+
+		// "Return"
+		".3: \n"
+		::
+			"i"(ubOffsA0), "i"(uwOffsModer), "i"(uwOffsOdr), "i"(uwOffsIdr)
+	);
+}
+#else
+static inline  __attribute__((always_inline)) void rtcReadWriteProcess(void) {
+	// uint32_t ulStatus = NRD_GPIO_Port->IDR;
+	// // NWR/NRD may not occur right on CS edge, thus check for both pins at once
+	// if((ulStatus & READ_MASK) == 0) {
+	// 	// Read desired register number from address lines
+	// 	// ARM uses byte addressing so shift a bit less
+	// 	uint32_t ulAddr = (ulStatus >> (A0_Pos - 2)) & (0xF << 2); // read addr asap
+	// 	// Output cached value
+	// 	D0_GPIO_Port->ODR = *(uint32_t*)&((uint8_t*)s_pMsmFields)[ulAddr];
+	// 	// Switch data to output
+	// 	D0_GPIO_Port->MODER = (
+	// 		MODE_OUT(D0_Pos + 0) | MODE_OUT(D0_Pos + 1) |
+	// 		MODE_OUT(D0_Pos + 2) | MODE_OUT(D0_Pos + 3)
+	// 	);
+	// 	// Switch data to input to not trash data lines
+	// 	D0_GPIO_Port->MODER = 0;
+	// }
+	// else if((ulStatus & WRITE_MASK) == 0) {
+	// 	// Write - TIME CRITICAL!
+	// 	uint32_t ulAddr = A0_GPIO_Port->IDR; // read addr asap
+	// 	uint32_t ulData = D0_GPIO_Port->IDR; // read data asap
+	// 	ulAddr >>=  A0_Pos;
+	// 	ulData >>= D0_Pos;
+	// 	// printf("WRITE: %x %x\r\n", ubAddr, ubData);
+	// }
+}
+#endif
+
+static const uint8_t s_pWeekStm2Msm[] = {0, 1, 2, 3, 4, 5, 6, 0};
+
+void msmRtcLoop(void) {
+#if defined(USE_ASM)
+	rtcInitAsm();
+	while(1) {
+		rtcReadWriteProcessAsm();
 	}
-	else
-	if((ulStatus & READ_MASK) == 0) {
+#else
+	uint32_t ulReg;
+	while(1) {
+		// TODO Shifts are different on different chips - overflows & underflows
+
+		// // Update seconds units
+		// rtcReadWriteProcess();
+		ulReg = RTC->TR;
+		// rtcReadWriteProcess();
+		s_pMsmFields[MSM_SECONDS_1] = ulReg << (D0_Pos - RTC_TR_SU_Pos);
+
+		// // Update seconds tens
+		// rtcReadWriteProcess();
+		// ulReg = RTC->TR;
+		// rtcReadWriteProcess();
+		// ulReg = ulReg << (D0_Pos - RTC_TR_ST_Pos);
+		// rtcReadWriteProcess();
+		// s_pMsmFields[MSM_SECONDS_10] = ulReg;
+
+		// // Update minutes units
+		// rtcReadWriteProcess();
+		// ulReg = RTC->TR;
+		// rtcReadWriteProcess();
+		// ulReg = ulReg >> (RTC_TR_MNU_Pos - D0_Pos);
+		// rtcReadWriteProcess();
+		// s_pMsmFields[MSM_MINUTES_1] = ulReg;
+
+		// // Update minutes tens
+		// rtcReadWriteProcess();
+		// ulReg = RTC->TR;
+		// rtcReadWriteProcess();
+		// ulReg = ulReg >> (RTC_TR_MNT_Pos - D0_Pos);
+		// rtcReadWriteProcess();
+		// s_pMsmFields[MSM_MINUTES_10] = ulReg;
+
+		// // Update hour units
+		// rtcReadWriteProcess();
+		// ulReg = RTC->TR;
+		// rtcReadWriteProcess();
+		// ulReg = ulReg >> (RTC_TR_HU_Pos - D0_Pos);
+		// rtcReadWriteProcess();
+		// s_pMsmFields[MSM_HOURS_1] = ulReg;
+
+		// // Update hour tens
+		// rtcReadWriteProcess();
+		// ulReg = RTC->TR;
+		// rtcReadWriteProcess();
+		// ulReg = ulReg >> (RTC_TR_HT_Pos - D0_Pos);
+		// rtcReadWriteProcess();
+		// s_pMsmFields[MSM_HOURS_10] = ulReg;
+
+		// // Read day/month/year/dayOfWeek
+		// rtcReadWriteProcess();
+		// ulReg = RTC->DR;
+
+		// // Update day
+		// rtcReadWriteProcess();
+		// s_pMsmFields[MSM_DAY_1] = ulReg << (D0_Pos - RTC_DR_DU_Pos);
+		// rtcReadWriteProcess();
+		// s_pMsmFields[MSM_DAY_10] = ulReg << (D0_Pos - RTC_DR_DT_Pos);
+
+		// // Update month
+		// rtcReadWriteProcess();
+		// s_pMsmFields[MSM_MONTH_1] = ulReg << (RTC_DR_MU_Pos - D0_Pos);
+		// rtcReadWriteProcess();
+		// ulVal = ulReg & RTC_DR_MT_Msk;
+		// s_pMsmFields[MSM_MONTH_10] = ulVal >> (RTC_DR_MT_Pos - D0_Pos);
+
+		// // Update week day
+		// // MSM: 0 is sunday, 1 is monday
+		// // STM: 0 is forbidden, 1 is monday, 7 is sunday
+		// rtcReadWriteProcess();
+		// ulVal = ulReg >> RTC_DR_WDU_Pos;
+		// rtcReadWriteProcess();
+		// ulVal &= 7;
+		// rtcReadWriteProcess();
+		// ulVal = s_pWeekStm2Msm[ulVal];
+		// rtcReadWriteProcess();
+		// s_pMsmFields[MSM_DAY_OF_WEEK] = ulVal << D0_Pos;
+
+		// // Update year
+		// rtcReadWriteProcess();
+		// s_pMsmFields[MSM_YEAR_1] = ulReg >> (RTC_DR_YU_Pos - D0_Pos);
+		// rtcReadWriteProcess();
+		// s_pMsmFields[MSM_YEAR_10] = ulReg >> (RTC_DR_YT_Pos - D0_Pos);
+	}
+#endif
+}
+
+void SysTick_Handler(void) {
+	// Systick interrupts are reason for timing errors occuring periodically
+	// so we're going to permanently disable it.
+	SysTick->CTRL = 0;    //Disable Systick
+}
+
+void EXTI4_15_IRQHandler(void) {
+	uint32_t ulStatus = A0_GPIO_Port->IDR;
+  if(__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_12)) {
+		// printf("READ\r\n");
 		// Read desired register number from address lines
 		// ARM uses byte addressing so shift a bit less
-		uint32_t ulAddr = (A0_GPIO_Port->IDR >> (A0_Pos - 2)) & (0xF << 2); // read addr asap
-		// printf("READ! %lu\r\n", ulAddr);
+		uint32_t ulAddr = (ulStatus >> (A0_Pos - 2)) & (0xF << 2); // read addr asap
 		// Output cached value
 		D0_GPIO_Port->ODR = *(uint32_t*)&((uint8_t*)s_pMsmFields)[ulAddr];
 		// Switch data to output
@@ -113,78 +306,16 @@ static inline __attribute__((always_inline)) void rtcReadWriteProcess(void) {
 		);
 		// Switch data to input to not trash data lines
 		D0_GPIO_Port->MODER = 0;
+		__HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_12);
+  }
+  if(__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_13)) {
+		// printf("WRITE\r\n");
+		// Write - TIME CRITICAL!
+		uint32_t ulAddr = A0_GPIO_Port->IDR; // read addr asap
+		uint32_t ulData = D0_GPIO_Port->IDR; // read data asap
+		ulAddr >>=  A0_Pos;
+		ulData >>= D0_Pos;
+		// printf("WRITE: %x %x\r\n", ubAddr, ubData);
+		__HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_13);
 	}
-}
-
-static const uint8_t pWeekStm2Msm[] = {0, 1, 2, 3, 4, 5, 6, 0};
-
-void msmRtcLoop(void) {
-	uint32_t ulReg, ulVal;
-	while(1) {
-		// TODO Shifts are different on different chips - overflows & underflows
-		// Get hours/minutes/seconds
-		rtcReadWriteProcess();
-		ulReg = RTC->TR;
-
-		// Update seconds
-		rtcReadWriteProcess();
-		s_pMsmFields[MSM_SECONDS_1] = ulReg << (D0_Pos - RTC_TR_SU_Pos);
-		rtcReadWriteProcess();
-		s_pMsmFields[MSM_SECONDS_10] = ulReg << (D0_Pos - RTC_TR_ST_Pos);
-
-		// Update minutes
-		rtcReadWriteProcess();
-		s_pMsmFields[MSM_MINUTES_1] = ulReg << (D0_Pos - RTC_TR_MNU_Pos);
-		rtcReadWriteProcess();
-		s_pMsmFields[MSM_MINUTES_10] = ulReg >> (RTC_TR_MNT_Pos - D0_Pos);
-
-		// Update hour
-		rtcReadWriteProcess();
-		s_pMsmFields[MSM_HOURS_1] = ulReg >> (RTC_TR_HU_Pos - D0_Pos);
-		rtcReadWriteProcess();
-		s_pMsmFields[MSM_HOURS_10] = ulReg >> (RTC_TR_HT_Pos - D0_Pos);
-
-		// Read day/month/year/dayOfWeek
-		rtcReadWriteProcess();
-		ulReg = RTC->DR;
-
-		// Update day
-		rtcReadWriteProcess();
-		s_pMsmFields[MSM_DAY_1] = ulReg << (D0_Pos - RTC_DR_DU_Pos);
-		rtcReadWriteProcess();
-		s_pMsmFields[MSM_DAY_10] = ulReg << (D0_Pos - RTC_DR_DT_Pos);
-
-		// Update month
-		rtcReadWriteProcess();
-		s_pMsmFields[MSM_MONTH_1] = ulReg << (D0_Pos - RTC_DR_MU_Pos);
-		rtcReadWriteProcess();
-		ulVal = ulReg & RTC_DR_MT_Msk;
-		s_pMsmFields[MSM_MONTH_10] = ulVal >> (RTC_DR_MT_Pos - D0_Pos);
-
-		// Update week day
-		// MSM: 0 is sunday, 1 is monday
-		// STM: 0 is forbidden, 1 is monday, 7 is sunday
-		rtcReadWriteProcess();
-		ulVal = ulReg >> RTC_DR_WDU_Pos;
-		rtcReadWriteProcess();
-		ulVal &= 7;
-		rtcReadWriteProcess();
-		ulVal = pWeekStm2Msm[ulVal];
-		rtcReadWriteProcess();
-		s_pMsmFields[MSM_DAY_OF_WEEK] = ulVal << D0_Pos;
-
-		// Update year
-		rtcReadWriteProcess();
-		s_pMsmFields[MSM_YEAR_1] = ulReg >> (RTC_DR_YU_Pos - D0_Pos);
-		rtcReadWriteProcess();
-		s_pMsmFields[MSM_YEAR_10] = ulReg >> (RTC_DR_YT_Pos - D0_Pos);
-	}
-}
-
-void SysTick_Handler(void) {
-	// Interrupts are reason for timing errors occuring periodically
-	// so we're going to permanently disable interrupts.
-	// Can be only done in supervisor mode, e.g. in interrupts themselves
-	// so let's do it here.
-	__disable_irq();
 }
